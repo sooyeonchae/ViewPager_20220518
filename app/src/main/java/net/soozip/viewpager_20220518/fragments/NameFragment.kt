0433c53f84690b10292c4cfeb497f2b71697bc25
채수연 <sooyeonchae@gmail.com>
2022-05-18 13:31:42 +0900

    프래그먼트 3개 추가 + ViewPager태그 배치.

---
+++ app/src/main/java/net/soozip/viewpager_20220518/fragments/NameFragment.kt
@@ -0,0 +1,28 @@
+package net.soozip.viewpager_20220518.fragments
+
+import android.os.Bundle
+import android.util.Log
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import net.soozip.viewpager_20220518.R
+
+class NameFragment : Fragment() {
+
+    //어떤 xml을 끌어다 사용할 지 결정하는 함수
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+
+        return inflater.inflate(R.layout.fragment_name, container, false)
+    }
+
+    override fun onActivityCreated(savedInstanceState: Bundle?) {
+        super.onActivityCreated(savedInstanceState)
+
+
+    }
+}
