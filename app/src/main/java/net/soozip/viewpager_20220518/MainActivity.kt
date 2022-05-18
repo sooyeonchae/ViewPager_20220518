4d4b8eccdbbb9e3e562790ba162aec4c13d7537c
채수연 <sooyeonchae@gmail.com>
2022-05-18 13:50:19 +0900

    참고사항 (재사용성) 코드

--- app/src/main/java/net/soozip/viewpager_20220518/MainActivity.kt
+++ app/src/main/java/net/soozip/viewpager_20220518/MainActivity.kt
@@ -2,10 +2,31 @@ package net.soozip.viewpager_20220518
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import kotlinx.android.synthetic.main.activity_main.*
+import net.soozip.viewpager_20220518.adapters.MainViewPagerAdapter
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var mAdapter : MainViewPagerAdapter
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        setupEvents()
+        setValues()
+
+    }
+
+    fun setupEvents() {
+
+    }
+
+    fun setValues(){
+        mAdapter = MainViewPagerAdapter( supportFragmentManager )
+        mainViewPager.adapter = mAdapter
+
+        //재사용성 억제 > 한번 만들어둔 프래그먼트 객체들을 유지.
+        mainViewPager.offscreenPageLimit = 3
     }
 }
