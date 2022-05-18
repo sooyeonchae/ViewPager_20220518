9ae4f500843c355ef3b6cd089a3510c5631196a4
채수연 <sooyeonchae@gmail.com>
2022-05-18 13:42:06 +0900

    뷰페이저용 어댑터 클래스 제작.

---
+++ app/src/main/java/net/soozip/viewpager_20220518/adapters/MainViewPagerAdapter.kt
@@ -0,0 +1,24 @@
+package net.soozip.viewpager_20220518.adapters
+
+import androidx.fragment.app.Fragment
+import androidx.fragment.app.FragmentManager
+import androidx.fragment.app.FragmentPagerAdapter
+import net.soozip.viewpager_20220518.fragments.HelloFragment
+import net.soozip.viewpager_20220518.fragments.NameFragment
+import net.soozip.viewpager_20220518.fragments.PhoneFragment
+
+class MainViewPagerAdapter(fm: FragmentManager) : FragmentPagerAdapter(fm) {
+
+    //몇 장짜리 뷰페이저인지 장수를 리턴.
+    override fun getCount() = 3
+
+    //각 위치에 맞는 Fragment 객체들을 리턴.
+    override fun getItem(position: Int): Fragment {
+        return when(position) {
+            0 -> NameFragment()
+            1 -> PhoneFragment()
+            else -> HelloFragment()
+        }
+    }
+
+}
